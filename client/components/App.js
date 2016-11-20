3ca066b7af1b81094dc4cbe3aa1ba77d543e1648
leviathan88 <levigithuber@gmail.com>
2016-11-20 10:07:27 +0100

    adding simple client

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>let me tell you a secret, pssst</div>
+    )
+  }
+}
+
+export default App
