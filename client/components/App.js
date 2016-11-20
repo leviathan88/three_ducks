d5e48a05692b2148028eef63e9d8488f7a1110cd
leviathan88 <levigithuber@gmail.com>
2016-11-20 11:37:37 +0100

    connecting to redux and mappint stateToProps

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import * as userActions from '../actions/userActions'
+
 class App extends Component {
+  componentWillMount() {
+    setTimeout(()=> {
+      this.props.changeName()
+    }, 3000)
+  }
+
   render() {
+    const { name } = this.props;
     return (
-      <div>let me tell you a secret, pssst</div>
+      <div className="container">
+        <h1>Hello from Redux</h1>
+        <p>my name is {name}</p>
+      </div>
     )
   }
 }
 
-export default App
+export default connect(mapStateToProps, userActions)(App)
+
+function mapStateToProps(state) {
+  return {
+    name: state.user.name,
+    married: state.user.married
+  }
+}
