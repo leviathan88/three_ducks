b78997829303049f62c879ebb2fbc7eeab60e4fe
leviathan88 <levigithuber@gmail.com>
2016-11-22 21:07:53 +0100

    adding router and bootstrap 3

--- client/components/App.js
+++ client/components/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react'
-
-import { connect } from 'react-redux'
-import * as userActions from '../actions/userActions'
+import NavigationBar from './NavigationBar'
 
 class App extends Component {
-  componentWillMount() {
-    setTimeout(()=> {
-      this.props.changeName()
-    }, 3000)
-  }
-
   render() {
-    const { name } = this.props;
     return (
       <div className="container">
-        <h1>Hello from Redux</h1>
-        <p>my name is {name}</p>
+        <NavigationBar/>
+        {this.props.children}
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps, userActions)(App)
-
-function mapStateToProps(state) {
-  return {
-    name: state.user.name,
-    married: state.user.married
-  }
-}
+export default App
--- client/index.js
+++ client/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
 
 import App from './components/App'
 import store from './store'
+import routes from './routes'
 
 render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
+  <Router history={browserHistory} routes={routes}/>
   , document.getElementById('app'))
